3a777efa260d2b743ef1c191d16fb9d4622e2caa
kevinsbstn4 <kevincaesar22@gmail.com>
2020-05-06 11:03:56 +0700

    navbar & jumbotron

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,89 +1,54 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI</h3>
-    <button type="button" class="btn btn-danger">Danger</button>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div class="main">
+    <!-- NAVBAR -->
+    <div class="nav1">
+      <div class="d-flex justify-content-end">
+        <div class="p-2 bd-highlight">Vonage for Home</div>
+        <div class="p-2 bd-highlight">Log In</div>
+        <div class="p-2 bd-highlight">Support</div>
+      </div>
+    </div>
+
+    <div class="nav2">
+      <nav class="navbar navbar-expand-lg ">
+        <img
+          style="width: 15%"
+          src="https://image4.owler.com/logo/vonage_owler_20191212_183242_original.jpg"
+        />
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a style="color: black;" class="nav-link" href="#">Products</a>
+            </li>
+            <li class="nav-item">
+              <a style="color: black;" class="nav-link" href="#">Resources</a>
+            </li>
+            <li class="nav-item">
+              <a style="color: black;" class="nav-link" href="#"
+                >Developer Center</a
+              >
+            </li>
+            <li class="nav-item">
+              <a style="color: black;" class="nav-link" href="#">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a style="color: black;" class="nav-link" href="#">Pricing</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <!-- NAVBAR END -->
+    <!-- JUMBOTRON -->
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Fluid jumbotron</h1>
+        <p class="lead">
+          This is a modified jumbotron that occupies the entire horizontal space
+          of its parent.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import "jquery";
 import "popper.js";
 import "bootstrap";
 import "./assets/app.scss";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faUserSecret);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 Vue.config.productionTip = false;
 
