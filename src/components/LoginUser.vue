0558fd32a50205d14cafbe4ae659e34496c1bdfd
kevinsbstn4 <kevincaesar22@gmail.com>
2020-05-19 11:35:58 +0700

    add form login

--- src/components/LoginUser.vue
+++ src/components/LoginUser.vue
@@ -1,6 +1,39 @@
 <template>
-  <div>
-    hello
+  <div class="main-login">
+    <div class="wrapper fadeInDown">
+      <div id="formContent">
+        <!-- Tabs Titles -->
+
+        <!-- Icon -->
+        <div class="fadeIn first">
+          <img
+            style="width: 30%; padding-top: 5%"
+            src="https://webstockreview.net/images/user-icon-png-5.png"
+            id="icon"
+            alt="User Icon"
+          />
+        </div>
+
+        <!-- Login Form -->
+        <form>
+          <input
+            type="text"
+            id="login"
+            class="fadeIn second"
+            name="login"
+            placeholder="Username"
+          />
+          <input
+            type="text"
+            id="password"
+            class="fadeIn third"
+            name="login"
+            placeholder="Password"
+          />
+          <input type="submit" class="fadeIn fourth" value="Log In" />
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 <script>
